fa1b1d3a8b2c6e6aa1867ad04c8e4bf3f3be12b4
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-01-30 11:01:31 +1100

    Cleaned up default minimal api template

---
+++ api/ConfigureApp.cs
@@ -0,0 +1,11 @@
+﻿namespace RFIDify;
+
+public static class ConfigureApp
+{
+    public static void Configure(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+    }
+}
---
+++ api/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿namespace RFIDify;
+
+public static class ConfigureServices
+{
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.AddSwagger();
+    }
+
+    private static void AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+}
