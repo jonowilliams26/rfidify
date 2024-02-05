c4888dd5a416486438cbc59e425ffd358dc20cbe
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-05 12:56:59 +1100

    Added edit/delete dropdown menu for rfid tags on the dashboard

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import * as Card from '$lib/components/ui/card';
-	import { Person, Link2 } from 'radix-icons-svelte';
+	import { Person, Link2, DotsHorizontal, Trash, Pencil1 } from 'radix-icons-svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 
 	export let data: PageData;
 </script>
@@ -9,13 +11,33 @@
 <div class="grid grid-cols-1 gap-1 md:grid-cols-2 xl:grid-cols-3">
 	{#each data.rfids as rfid}
 		<Card.Root>
-			<Card.Content class="flex space-x-2 p-4">
+			<Card.Content class="flex space-x-3 p-4 text-xs">
 				<img src={rfid.image} alt={rfid.name} class="h-16 w-16" />
-				<div class="truncate">
-					<h2 class="truncate pb-1 text-xs font-semibold">{rfid.name}</h2>
-					<div class="flex flex-col space-y-1 text-xs text-muted-foreground">
+				<div class="flex w-full flex-col truncate">
+					<div class="flex w-full items-start justify-between space-x-4 truncate pb-0.5">
+						<h2 class="truncate text-sm font-semibold">{rfid.name}</h2>
+						<DropdownMenu.Root>
+							<DropdownMenu.Trigger asChild let:builder>
+								<Button builders={[builder]} variant="ghost" size="icon" class="h-6 w-6">
+									<DotsHorizontal />
+								</Button>
+							</DropdownMenu.Trigger>
+							<DropdownMenu.Content>
+								<DropdownMenu.Item>
+									<Pencil1 class="mr-1.5" />
+									Edit
+								</DropdownMenu.Item>
+								<DropdownMenu.Separator />
+								<DropdownMenu.Item>
+									<Trash class="mr-1.5" />
+									Delete
+								</DropdownMenu.Item>
+							</DropdownMenu.Content>
+						</DropdownMenu.Root>
+					</div>
+					<div class="text-xs text-muted-foreground truncate space-y-1">
 						{#if rfid.type === 'Track' || rfid.type === 'Album'}
-							<div class="flex items-center space-x-1">
+							<div class="flex space-x-1">
 								<Person />
 								<span>{rfid.artists}</span>
 							</div>
@@ -23,10 +45,10 @@
 						{#if rfid.type === 'Playlist'}
 							<span class="truncate">{rfid.description}</span>
 						{/if}
-						<div class="flex items-center space-x-1">
-							<Link2 />
-							<span>{rfid.rfid}</span>
-						</div>
+                        <div class="flex space-x-1">
+                            <Link2 />
+                            <span>{rfid.rfid}</span>
+                        </div>
 					</div>
 				</div>
 			</Card.Content>
