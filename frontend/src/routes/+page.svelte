4a47a89c73f095dc88adf256fab5649b5ec77494
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-09 08:31:50 +1100

    added shadcn svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
     import type { PageData } from './$types';
-    import { PUBLIC_BASE_URL } from '$env/static/public';
+    import { PUBLIC_BASE_API_URL } from '$env/static/public';
     
     export let data: PageData;
 
     let rfids = [];
 
     onMount(async () => {
-        console.log('on Mount', PUBLIC_BASE_URL);
-        const response = await fetch(`${PUBLIC_BASE_URL}/rfids`);
+        console.log('on Mount', PUBLIC_BASE_API_URL);
+        const response = await fetch(`${PUBLIC_BASE_API_URL}/rfids`);
         rfids = await response.json();
     });
 </script>
 
 <h1>Hello from test</h1>
-<p>{PUBLIC_BASE_URL}</p>
+<p>{PUBLIC_BASE_API_URL}</p>
 <p>{rfids}</p>
