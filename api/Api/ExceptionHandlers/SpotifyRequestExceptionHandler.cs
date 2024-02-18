cd52b957399cfc859f6de5290d04367364325d55
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-18 16:11:39 +1100

    Added exception handlers for when Spotify returns a HTTP error or an unexpected exception is thrown

---
+++ api/Api/ExceptionHandlers/SpotifyRequestExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace RFIDify.Api.ExceptionHandlers;
+
+public class SpotifyRequestExceptionHandler(ILogger<SpotifyRequestExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception is HttpRequestException httpException)
+        {
+            logger.LogError(httpException, "An error occurred while making a request to the Spotify API.");
+            httpContext.Response.StatusCode = (int)(httpException.StatusCode ?? HttpStatusCode.InternalServerError);
+            await httpContext.Response.WriteAsJsonAsync(new
+            {
+                Message = "An error occurred while making a request to the Spotify API.",
+            }, cancellationToken);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ api/Api/ExceptionHandlers/UnhandledExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace RFIDify.Api.ExceptionHandlers;
+
+public class UnhandledExceptionHandler(ILogger<UnhandledExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, "An unhandled exception occurred.");
+        httpContext.Response.StatusCode = 500;
+        await httpContext.Response.WriteAsJsonAsync(new
+        {
+            Message = "Sorry, something went wrong",
+        }, cancellationToken);
+        return true;
+    }
+}
