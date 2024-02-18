b268f7be3421bc482f606736ee33a842edc21c70
Jonathan Williams <37890156+jonathanjameswilliams26@users.noreply.github.com>
2024-02-19 08:12:43 +1100

    If there are no rfids returned from the backend, the user will be redirected to the RFID setup page (#21)

--- frontend/src/routes/(app)/+page.ts
+++ frontend/src/routes/(app)/+page.ts
@@ -1,5 +1,5 @@
 import { getRFIDs } from '$lib/api/endpoints';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
@@ -7,6 +7,10 @@ export const load = (async ({ fetch }) => {
     const response = await getRFIDs(fetch);
     if (!response.ok) {
         error(500, 'Failed to load RFIDs');
+    }
+
+    if (response.data.length === 0) {
+        redirect(307, '/rfid/setup')
     }
 
     return {
