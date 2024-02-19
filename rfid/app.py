0e4296813806b26ee18798cae6c78b41a7afc8e4
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-19 13:51:30 +1100

    Refactored

--- rfid/app.py
+++ rfid/app.py
@@ -4,12 +4,23 @@ from PiicoDev_Unified import sleep_ms
 from requests import post
 from PiicoDev_Buzzer import PiicoDev_Buzzer
 
+# Constants
+last_seen_threshold_seconds = 1
+url = "http://api:8080/rfids/scan"
+max_buzzer_volume = 2
+buzz_duration_ms = 100
+buzz_frequency_hz = 1000
+ok = 200
+not_found = 404
+
 rfid_module = PiicoDev_RFID()
 buzzer = PiicoDev_Buzzer()
-url = "http://api:8080/rfids/scan"
+buzzer.volume(max_buzzer_volume)
 last_seen_rfid = None
 last_seen_at = None
-last_seen_threshold_seconds = 1
+
+def beep():
+    buzzer.tone(buzz_frequency_hz, buzz_duration_ms)
 
 print("Starting RFID scanner...")
 while True:
@@ -37,15 +48,15 @@ while True:
     last_seen_rfid = rfid
     last_seen_at = datetime.now()
     data = { "id": rfid }
-
     response = post(url, json=data)
-    if response.status_code == 200:
+
+    if response.status_code == ok:
         print(f'Successfully started playing RFID: {rfid}')
-        buzzer.tone(800, 500)
-    elif response.status_code == 404:
+        beep()
+    elif response.status_code == not_found:
         print(f'RFID: {rfid} not found')
-        buzzer.tone(800, 500)
+        beep()
     else:
-        print(f'Failed to start playing RFID: {rfid}')
+        print(f'An unexpected error occurred trying to send RFID: {rfid} response: {response}')
     
     sleep_ms(100)
