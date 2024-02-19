10f0ab904945332888be4f20d071cf1993be2c50
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-19 13:34:36 +1100

    Fixed bugs

--- rfid/app.py
+++ rfid/app.py
@@ -11,24 +11,19 @@ last_seen_rfid = None
 last_seen_at = None
 last_seen_threshold_seconds = 1
 
-def main():
-    print("Starting RFID scanner...")
-    while True:
-        scan_rfid()
-        sleep_ms(100)
-
-def scan_rfid():
+print("Starting RFID scanner...")
+while True:
     tag_present = rfid_module.tagPresent();
-    
+
     if not tag_present:
         sleep_ms(100)
-        return
+        continue
 
     result = rfid_module.readID(detail=True)
 
     if not result['success']:
         print("Failed to read RFID tag")
-        return
+        continue
 
     rfid = result['id_formatted']
 
@@ -36,7 +31,7 @@ def scan_rfid():
 
     if rfid == last_seen_rfid and below_last_seen_threshold:
         print(f'RFID: {rfid} already scanned within the last {last_seen_threshold_seconds} seconds. Skipping.')
-        return
+        continue
 
     print(f'Sending RFID: {rfid}')
     last_seen_rfid = rfid
@@ -46,15 +41,11 @@ def scan_rfid():
     response = post(url, json=data)
     if response.status_code == 200:
         print(f'Successfully started playing RFID: {rfid}')
-        buzz()
+        buzzer.tone(800, 500)
     elif response.status_code == 404:
         print(f'RFID: {rfid} not found')
-        buzz()
+        buzzer.tone(800, 500)
     else:
         print(f'Failed to start playing RFID: {rfid}')
-
-def buzz():
-    buzzer.tone(800, 500)
-
-if __name__ == '__main__':
-    main()
+    
+    sleep_ms(100)
