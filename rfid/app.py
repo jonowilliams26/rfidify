8b07ba22950c2ca2d63fbe94d5b7bfe260074e4b
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-19 13:27:39 +1100

    Added buzzer

--- rfid/app.py
+++ rfid/app.py
@@ -2,28 +2,33 @@ from datetime import datetime
 from PiicoDev_RFID import PiicoDev_RFID
 from PiicoDev_Unified import sleep_ms
 from requests import post
+from PiicoDev_Buzzer import PiicoDev_Buzzer
 
-print("Starting RFID scanner...")
 rfid_module = PiicoDev_RFID()
+buzzer = PiicoDev_Buzzer()
 url = "http://api:8080/rfids/scan"
 last_seen_rfid = None
 last_seen_at = None
 last_seen_threshold_seconds = 1
 
-print('Place tag near the PiicoDev RFID Module')
+def main():
+    print("Starting RFID scanner...")
+    while True:
+        scan_rfid()
+        sleep_ms(100)
 
-while True:
+def scan_rfid():
     tag_present = rfid_module.tagPresent();
     
     if not tag_present:
         sleep_ms(100)
-        continue
+        return
 
     result = rfid_module.readID(detail=True)
 
     if not result['success']:
         print("Failed to read RFID tag")
-        continue
+        return
 
     rfid = result['id_formatted']
 
@@ -31,8 +36,7 @@ while True:
 
     if rfid == last_seen_rfid and below_last_seen_threshold:
         print(f'RFID: {rfid} already scanned within the last {last_seen_threshold_seconds} seconds. Skipping.')
-        sleep_ms(100)
-        continue
+        return
 
     print(f'Sending RFID: {rfid}')
     last_seen_rfid = rfid
@@ -42,9 +46,15 @@ while True:
     response = post(url, json=data)
     if response.status_code == 200:
         print(f'Successfully started playing RFID: {rfid}')
+        buzz()
     elif response.status_code == 404:
         print(f'RFID: {rfid} not found')
+        buzz()
     else:
         print(f'Failed to start playing RFID: {rfid}')
 
-    sleep_ms(100)
+def buzz():
+    buzzer.tone(800, 500)
+
+if __name__ == '__main__':
+    main()
