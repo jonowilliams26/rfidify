0e7052356d59102a141558aed7249f794641ca08
jonathanjameswilliams26 <jonathanjameswilliams26@gmail.com>
2024-02-19 14:15:10 +1100

    Refactored with code documentation

--- rfid/app.py
+++ rfid/app.py
@@ -4,7 +4,8 @@ from PiicoDev_Unified import sleep_ms
 from requests import post
 from PiicoDev_Buzzer import PiicoDev_Buzzer
 
-# Constants
+# Contants
+# ------------------------------
 last_seen_threshold_seconds = 1
 url = "http://api:8080/rfids/scan"
 max_buzzer_volume = 2
@@ -13,50 +14,74 @@ buzz_frequency_hz = 1000
 ok = 200
 not_found = 404
 
+
+# Variables
+# ------------------------------
 rfid_module = PiicoDev_RFID()
 buzzer = PiicoDev_Buzzer()
 buzzer.volume(max_buzzer_volume)
 last_seen_rfid = None
 last_seen_at = None
 
-def beep():
-    buzzer.tone(buzz_frequency_hz, buzz_duration_ms)
 
-print("Starting RFID scanner...")
-while True:
+# Functions
+# ------------------------------
+def read_rfid() -> str | None:
+    """Reads the RFID from the RFID module
+
+    Returns:
+        str : The RFID read from the module
+        None : If no RFID is present, failed to read the RFID, or the RFID has been scanned within the last last_seen_threshold_seconds
+   """
     tag_present = rfid_module.tagPresent();
 
     if not tag_present:
-        sleep_ms(100)
-        continue
+        return None
 
     result = rfid_module.readID(detail=True)
 
     if not result['success']:
         print("Failed to read RFID tag")
-        continue
+        return None
 
     rfid = result['id_formatted']
+    print(f'Successfully read RFID: {rfid} from RFID module')
 
     below_last_seen_threshold = last_seen_at is not None and (datetime.now() - last_seen_at).total_seconds() < last_seen_threshold_seconds
 
     if rfid == last_seen_rfid and below_last_seen_threshold:
         print(f'RFID: {rfid} already scanned within the last {last_seen_threshold_seconds} seconds. Skipping.')
-        continue
+        return None
 
-    print(f'Sending RFID: {rfid}')
     last_seen_rfid = rfid
     last_seen_at = datetime.now()
+    return rfid
+
+
+def send_rfid(rfid: str | None):
+    """
+    Sends the RFID to the API
+
+    Args:
+        rfid (str | None): The RFID to send to the API, if None, the function will return without sending the RFID
+    """
+    if rfid is None:
+        return
+
+    print(f'Sending RFID: {rfid}')
     data = { "id": rfid }
     response = post(url, json=data)
 
-    if response.status_code == ok:
-        print(f'Successfully started playing RFID: {rfid}')
-        beep()
-    elif response.status_code == not_found:
-        print(f'RFID: {rfid} not found')
-        beep()
+    if response.status_code == ok or response.status_code == not_found:
+        print(f'Successfully sent RFID: {rfid}')
+        buzzer.tone(buzz_frequency_hz, buzz_duration_ms)
     else:
         print(f'An unexpected error occurred trying to send RFID: {rfid} response: {response}')
-    
+
+
+# Main
+print("Starting RFID scanner...")
+while True:
+    rfid = read_rfid()
+    send_rfid(rfid)
     sleep_ms(100)
