50008e7f84d142b1e3b917c175793561440c3424
Jonathan Williams <37890156+jonathanjameswilliams26@users.noreply.github.com>
2024-02-20 17:01:55 +1100

    Feature/icons (#28)
    * Added icons
    
    * Updated icon for add rfid button

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
-	import { Rocket } from 'radix-icons-svelte';
+	import { LinkBreak2 } from 'radix-icons-svelte';
 </script>
 
 <div class="min-h-full">
@@ -8,11 +8,11 @@
 		<div class="mx-auto max-w-7xl px-2">
 			<div class="flex h-16 items-center justify-between">
 				<a href="/" class="flex items-center space-x-2">
-					<img class="block h-8 w-auto" src="/spotify-logo.png" alt="logo" />
+					<img class="block h-8 w-auto" src="/icon.svg" alt="logo" />
                     <span class="text-primary font-bold text-lg">RFIDify</span>
 				</a>
 				<Button href="/rfid/setup">
-					<Rocket class="mr-2" />
+					<LinkBreak2 class="mr-2" />
 					Add RFID
 				</Button>
 			</div>
